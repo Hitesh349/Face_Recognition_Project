7b20265ccd7628c173a91e4dcaba8b04c2658288
HITESH MEHER <144425208+Hitesh349@users.noreply.github.com>
2024-11-21 03:55:40 +0530

    Update Data Visualization(Image recognition).py

--- Data Visualization/Data Visualization(Image recognition).py
+++ Data Visualization/Data Visualization(Image recognition).py
@@ -8,7 +8,17 @@ def visualize_detection(frame, detections, h, w):
         # extract the confidence (i.e., probability) associated with the prediction
         confidence = detections[0, 0, i, 2]
 
-…                          (0, 0, 255), 2)  # Draw the bounding box
+        # filter out weak detections by ensuring the `confidence` is greater than the minimum confidence
+        if confidence > 0.5:
+            # compute the (x, y)-coordinates of the bounding box for the face
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])  # Scale to original image size
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # draw the bounding box of the face along with the associated probability
+            text = "{:.2f}%".format(confidence * 100)
+            y = startY - 10 if startY - 10 > 10 else startY + 10
+            cv2.rectangle(frame, (startX, startY), (endX, endY),
+                          (0, 0, 255), 2)  # Draw the bounding box
             cv2.putText(frame, text, (startX, y),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)  # Add text label
 
